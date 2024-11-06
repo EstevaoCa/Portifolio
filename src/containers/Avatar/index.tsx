7df07692d21e241d8a28856265934f57b4da4be9
EstevaoCa <tevo.castro14@gmail.com>
2024-11-06 08:27:38 -0300

    Estilizando Sidebar

---
+++ src/containers/Avatar/index.tsx
@@ -0,0 +1,4 @@
+import { FotoPerfil } from './style'
+const Avatar = () => <FotoPerfil src="https://github.com/EstevaoCa.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import Avatar from '../Avatar'
+
+import { Descricao, BotaoTema, SidebarContainer } from './style'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/EstevaoCa.png" alt="Foto do perfil" />
-    <Titulo fontSize={16}>Estevao Castro</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={16}>Estevao Castro</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        EstevaoCa
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front End
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
